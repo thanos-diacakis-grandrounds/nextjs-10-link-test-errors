8c9cb466d62a303899c306f33288fd172683e197
Thanos Diacakis <thanos.diacakis@grandrounds.com>
2020-12-10 01:47:34 +0200

    Added comment to clarify NextJS versions.

--- component/TestComponent.test.jsx
+++ component/TestComponent.test.jsx
@@ -4,6 +4,7 @@ import {render, act} from '@testing-library/react';
 describe('<TestComponent/>', () => {
 
     // Commenting out "test a" makes the problem go away.
+    // This fails with NextJS 10.x, but works with 9.5.6.
     it('test a', () => {
     });
 
